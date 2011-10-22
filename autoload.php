3c0f8fb43d113a25349080a4e6ce623f574f34a4
Tyler Akins <fidian@rumkin.com>
2011-10-22 12:31:49 -0500

    Forgot that they are all in subdirectories

--- autoload.php
+++ autoload.php
@@ -4,10 +4,22 @@ spl_autoload_register(function ($name) {
 	$filename = '_' . strtolower($name) . '.class.php';
 	$filename = str_replace('_', PATH_SEPARATOR, $filename);
 	$filename = __DIR__ . $filename;
+	$paths = array(
+		'db',
+		'dependencyinjectioncontainer',
+		'dump',
+		'friend',
+		'session2',
+		'tokenizer',
+		'webrequest',
+	);
 
-	if (file_exists($filename)) {
-		require_once($filename);
-		return true;
+	foreach ($paths as $path) {
+		$fullFile = $path . PATH_SEPARATOR . $filename;
+		if (file_exists($fullFile)) {
+			require_once($fullFile);
+			return true;
+		}
 	}
 
 	return false;
