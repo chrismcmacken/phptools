266c5ea451da9221e7643fd7a938df9a0105dd7a
Tyler Akins <fidian@rumkin.com>
2012-04-16 11:04:51 -0500

    Use objects instead of remembering array offsets

--- tokenizer/tokenizer.class.php
+++ tokenizer/tokenizer.class.php
@@ -40,15 +40,6 @@
  * token name instead of consistently using English for everything.  This
  * tokenizer class helps make everything the same.
  *
- * Standardized tokens all look like this:
- *
- *   array(
- *     [0] => T_TOKEN_CONSTANT,
- *     [1] => 'token content',
- *     [2] => 77,  // Line number
- *     [3] => 28,  // Matching token offset or null if no match
- *   )
- *
  * The matching token offset would give you the index of a '{' if you were
  * at a '}'.  Usually it will be the same as the key of the token.  Only for
  * special tokens that could have a match would it be different.
@@ -59,62 +50,19 @@
  * prefer to call isset() instead of $this->valid().
  */
 
+require_once(__DIR__ . '/tokenizertoken.class.php');
+
 class Tokenizer implements ArrayAccess, Iterator {
 	const EXCEPTION_EXTENSION = 297671;
 	const EXCEPTION_GETTOKEN = 635090;
 	const EXCEPTION_OFFSETSET = 200469;
 	const EXCEPTION_OFFSETUNSET = 277193;
 
-	static protected $customTokens = array(
-		// Eliminate the only Hebrew token
-		T_PAAMAYIM_NEKUDOTAYIM => 'T_DOUBLE_COLON',
-
-		// Default token and ones that don't match strings
-		-1 => 'T_TOKENIZER_DEFAULT',
-		-2 => 'T_TOKENIZER_BACKTICK_LEFT',
-		-3 => 'T_TOKENIZER_BACKTICK_RIGHT',
-
-		// Ones that don't come back as arrays
-		'`' => 'T_TOKENIZER_BACKTICK',
-		'~' => 'T_TOKENIZER_BITWISE_NOT',
-		'^' => 'T_TOKENIZER_BITWISE_XOR',
-		'!' => 'T_TOKENIZER_BOOLEAN_NEGATION',
-		'{' => 'T_TOKENIZER_BRACE_LEFT',
-		'}' => 'T_TOKENIZER_BRACE_RIGHT',
-		'[' => 'T_TOKENIZER_BRACKET_LEFT',
-		']' => 'T_TOKENIZER_BRACKET_RIGHT',
-		':' => 'T_TOKENIZER_COLON',
-		',' => 'T_TOKENIZER_COMMA',
-		'.' => 'T_TOKENIZER_CONCAT',
-		'"' => 'T_TOKENIZER_DOUBLE_QUOTE',
-		'=' => 'T_TOKENIZER_EQUAL',
-		'>' => 'T_TOKENIZER_IS_GREATER_THAN',
-		'<' => 'T_TOKENIZER_IS_LESS_THAN',
-		'/' => 'T_TOKENIZER_MATH_DIVIDE',
-		'-' => 'T_TOKENIZER_MATH_MINUS',
-		'*' => 'T_TOKENIZER_MATH_MULTIPLY',
-		'%' => 'T_TOKENIZER_MATH_MODULUS',
-		'+' => 'T_TOKENIZER_MATH_PLUS',
-		'(' => 'T_TOKENIZER_PAREN_LEFT',
-		')' => 'T_TOKENIZER_PAREN_RIGHT',
-		'&' => 'T_TOKENIZER_REFERENCE',
-		';' => 'T_TOKENIZER_SEMICOLON',
-		'@' => 'T_TOKENIZER_SUPPRESS_ERRORS',
-		'?' => 'T_TOKENIZER_TERNARY',
-		'$' => 'T_TOKENIZER_VARIABLE_VARIABLE',
-
-		// T_STRING substitutions into tokens
-		'define' => 'T_TOKENIZER_DEFINE',
-		'false' => 'T_TOKENIZER_FALSE',
-		'true' => 'T_TOKENIZER_TRUE',
-	);
-
 	protected $currentToken = 0;
 	protected $isValid = true;
 	protected $isValidReason = null;
 	protected $filename = null;
 	protected $tokens = array();
-	static protected $unknownTokenName = null;
 
 
 	/**
@@ -157,7 +105,7 @@ class Tokenizer implements ArrayAccess, Iterator {
 		$indices = array();
 
 		foreach ($this->tokens as $k => $v) {
-			if (in_array($v[0], $tokenList)) {
+			if (in_array($v->type, $tokenList)) {
 				$indices[] = $k;
 			}
 		}
@@ -178,32 +126,30 @@ class Tokenizer implements ArrayAccess, Iterator {
 
 
 	/**
-	 * Gets the constant value for a given token.
+	 * Increment our index and return the new current token, skipping ones
+	 * that are not important
 	 *
-	 * @param array $token
-	 * @return value that matches a T_* constant
+	 * @return array|null Null if no more tokens
 	 */
-	public function getConstant($token) {
-		if (! array_key_exists(0, $token)) {
+	public function getImportantTokenIndex($increment, $startAt = null) {
+		if (is_null($startAt)) {
+			$startAt = $this->currentToken;
+		}
+
+		$offset = $startAt + $increment;
+
+		$token = $this->getTokenAt($offset);
+		
+		while (! is_null($token) && ! $token->isImportant()) {
+			$offset += $increment;
+			$token = $this->getTokenAt($offset);
+		}
+
+		if (is_null($token)) {
 			return null;
 		}
 
-		return $token[0];
-	}
-
-
-	/**
-	 * Gets the content for a given token.
-	 *
-	 * @param array $token
-	 * @return value Content of the token
-	 */
-	public function getContent($token) {
-		if (! array_key_exists(1, $token)) {
-			return null;
-		}
-
-		return $token[1];
+		return $offset;
 	}
 
 
@@ -218,92 +164,6 @@ class Tokenizer implements ArrayAccess, Iterator {
 
 
 	/**
-	 * Gets the line number for a token passed in
-	 *
-	 * @param array $token
-	 * @return integer
-	 */
-	public function getLine($token) {
-		return $token[2];
-	}
-
-
-	/**
-	 * Gets the match for a token passed in.  If there is no need for
-	 * a match, return null.  If no match was found, return false.
-	 *
-	 * @param array $token
-	 * @return integer|null|false
-	 */
-	public function getMatch($token) {
-		if (! array_key_exists(3, $token)) {
-			return null;
-		}
-
-		return $token[3];
-	}
-
-
-	/**
-	 * Gets the name for a token or for the token at an offset
-	 *
-	 * @param mixed $token Token array or token constant
-	 * @return string|null
-	 */
-	public function getName($token) {
-		if (is_array($token)) {
-			$token = $token[0];
-		}
-
-		if (! empty(static::$customTokens[$token])) {
-			return static::$customTokens[$token];
-		}
-
-		$name = token_name($token);
-
-		if ($name === static::$unknownTokenName) {
-			$name = 'UNKNOWN(' . $token . ')';
-		}
-
-		return $name;
-	}
-
-
-	/**
-	 * Get the name of the token at the specified index
-	 *
-	 * @param integer $index Array key (direct access)
-	 * @return string|null Null if no token at index
-	 */
-	public function getNameAt($index) {
-		$token = $this->getTokenAt($index);
-
-		if (is_null($token)) {
-			return null;
-		}
-
-		return $this->getName($token);
-	}
-
-
-	/**
-	 * Get the name of the token relative to the current token
-	 *
-	 * @param integer $offset Amount to jump away from current pointer
-	 * @return string|null Null if no token at index
-	 */
-	public function getNameRelative($offset) {
-		$token = $this->getTokenRelative($offset);
-
-		if (is_null($token)) {
-			return null;
-		}
-
-		return $this->getName($token);
-	}
-
-
-	/**
 	 * Increment our index and return the new current token, skipping ones
 	 * that are not important
 	 *
@@ -313,7 +173,7 @@ class Tokenizer implements ArrayAccess, Iterator {
 		$this->currentToken ++;
 		$token = $this->getTokenAt($this->currentToken);
 
-		while (! is_null($token) && ! $this->isImportant($token)) {
+		while (! is_null($token) && ! $token->isImportant()) {
 			$this->currentToken ++;
 			$token = $this->getTokenAt($this->currentToken);
 		}
@@ -343,7 +203,7 @@ class Tokenizer implements ArrayAccess, Iterator {
 		$this->currentToken --;
 		$token = $this->getTokenAt($this->currentToken);
 
-		while (! is_null($token) && ! $this->isImportant($token)) {
+		while (! is_null($token) && ! $token->isImportant()) {
 			$this->currentToken --;
 			$token = $this->getTokenAt($this->currentToken);
 		}
@@ -407,49 +267,13 @@ class Tokenizer implements ArrayAccess, Iterator {
 	}
 
 
+	/**
+	 * Make sure that the tokenizer functions exist
+	 */
 	static public function initialize() {
-		// Check that tokenizer is loaded
 		if (! extension_loaded('tokenizer')) {
 			throw new Exception('Missing the tokenizer extension', self::EXCEPTION_EXTENSION);
 		}
-
-		foreach (static::$customTokens as $value => $name) {
-			if (! defined($name)) {
-				define($name, $value);
-			}
-		}
-
-		/* Set the actual unknown token name in case it's different
-		 * due to version changes or localization.
-		 */
-		static::$unknownTokenName = token_name(T_TOKENIZER_DEFAULT);
-	}
-
-
-	/**
-	 * Returns true if the current token is not one of the following:
-	 *
-	 * T_COMMENT
-	 * T_DOC_COMMENT
-	 * T_WHITESPACE
-	 *
-	 * @param array|null|mixed $token Token array, constant or null for current
-	 * @return boolean True if this token "counts" for processing
-	 */
-	public function isImportant($token = null) {
-		if (is_null($token)) {
-			$token = $this->tokens[$this->currentToken][0];
-		}
-
-		if (is_array($token)) {
-			$token = $token[0];
-		}
-
-		if ($token == T_COMMENT || $token == T_DOC_COMMENT || $token == T_WHITESPACE) {
-			return false;
-		}
-
-		return true;
 	}
 
 
@@ -549,6 +373,16 @@ class Tokenizer implements ArrayAccess, Iterator {
 
 
 	/**
+	 * Go to a specific index
+	 *
+	 * @param integer $index
+	 */
+	public function setIndex($index) {
+		$this->currentToken = (integer) $index;
+	}
+
+
+	/**
 	 * Sets the reason that the tokenizer believes the code is invalid.
 	 * Only keeps around the first reason.
 	 *
@@ -571,94 +405,60 @@ class Tokenizer implements ArrayAccess, Iterator {
 	 * @return array Standardized list of tokens
 	 */
 	protected function standardizeTokens($tokens) {
-		$lastLineNumber = 1;
+		$lastToken = null;
 		$matchStack = array();
 		$matchStackPhp = array();
 		$backtickIsLeft = true;
+		TokenizerToken::reset();
+		$tokenObjects = array();
 
 		foreach ($tokens as $key => $token) {
-			if (! is_array($token)) {
-				// Convert to array
-				$tokenValue = $token;
+			$token = TokenizerToken::create($token, $lastToken);
+			$tokenObjects[] = $token;
+			$lastToken = $token;
 
-				if (! isset(static::$customTokens[$tokenValue])) {
-					$tokenValue = T_TOKENIZER_DEFAULT;
-				}
-
-				$token = array(
-					$tokenValue,
-					$token,
-					$lastLineNumber
-				);
-				$tokens[$key] = $token;  // Save back to original array
-			} elseif (T_STRING === $token[0]) {
-				// Convert some strings into tokens
-				$lowercase = strtolower($token[1]);
-
-				if (isset(self::$customTokens[$lowercase])) {
-					$token[0] = $lowercase;
-					$tokens[$key] = $token;  // Save back to original array
-				}
-			}
-
-			if ($token[0] == T_TOKENIZER_BACKTICK) {
-				if ($backtickIsLeft) {
-					$tokens[$key][0] = T_TOKENIZER_BACKTICK_LEFT;
-				} else {
-					$tokens[$key][0] = T_TOKENIZER_BACKTICK_RIGHT;
-				}
-
-				$token[0] = $tokens[$key][0];
-				$backtickIsLeft = ! $backtickIsLeft;
-			}
-
-			$matchOpen = function ($find) use (&$tokens, $key, &$matchStack) {
-				$tokens[$key][3] = false;
-				$matchStack[] = array($find, $key);
-			};
-
-			switch ($token[0]) {
+			switch ($token->type) {
 				case T_OPEN_TAG:
 				case T_OPEN_TAG_WITH_ECHO:
-					$tokens[$key][3] = false;
-					$matchStackPhp[] = $key;
+					$token->setMatch(false);
+					$matchStackPhp[] = array($key, $token);
 					break;
 
 				case T_CLOSE_TAG:
 					if ($this->isValid) {
 						if (! count($matchStackPhp)) {
-							$tokens[$key][3] = false;
+							$token->setMatch(false);
 							$this->setReason('Trying to match ' . $this->tokenToString($token) . ' with nothing');
 						} else {
 							$match = array_pop($matchStackPhp);
-							$tokens[$key][3] = $match;
-							$tokens[$match][3] = $key;
+							$token->setMatch($match[0]);
+							$match[1]->setMatch($key);
 						}
 					} else {
-						$tokens[$key][3] = false;
+						$token->setMatch(false);
 					}
 					break;
 
 				case T_CURLY_OPEN:
 				case T_DOLLAR_OPEN_CURLY_BRACES:
 				case T_TOKENIZER_BRACE_LEFT:
-					$tokens[$key][3] = false;
-					$matchStack[] = array(T_TOKENIZER_BRACE_RIGHT, $key);
+					$token->setMatch(false);
+					$matchStack[] = array(T_TOKENIZER_BRACE_RIGHT, $key, $token);
 					break;
 
 				case T_TOKENIZER_BACKTICK_LEFT:
-					$tokens[$key][3] = false;
-					$matchStack[] = array(T_TOKENIZER_BACKTICK_RIGHT, $key);
+					$token->setMatch(false);
+					$matchStack[] = array(T_TOKENIZER_BACKTICK_RIGHT, $key, $token);
 					break;
 
 				case T_TOKENIZER_BRACKET_LEFT:
-					$tokens[$key][3] = false;
-					$matchStack[] = array(T_TOKENIZER_BRACKET_RIGHT, $key);
+					$token->setMatch(false);
+					$matchStack[] = array(T_TOKENIZER_BRACKET_RIGHT, $key, $token);
 					break;
 
 				case T_TOKENIZER_PAREN_LEFT:
-					$tokens[$key][3] = false;
-					$matchStack[] = array(T_TOKENIZER_PAREN_RIGHT, $key);
+					$token->setMatch(false);
+					$matchStack[] = array(T_TOKENIZER_PAREN_RIGHT, $key, $token);
 					break;
 
 				case T_TOKENIZER_BACKTICK_RIGHT:
@@ -667,30 +467,28 @@ class Tokenizer implements ArrayAccess, Iterator {
 				case T_TOKENIZER_PAREN_RIGHT:
 					if ($this->isValid) {
 						$match = array_pop($matchStack);
-						if ($token[0] != $match[0]) {
-							$tokens[$key][3] = false;
-							$this->setReason('Trying to match ' . $this->tokenToString($token) . ' with ' . $this->tokenToString($tokens[$match[1]]));
+						if ($token->type != $match[0]) {
+							$token->setMatch(false);
+							$this->setReason('Trying to match ' . $token . ' with ' . $match[2]);
 						} else {
-							$tokens[$key][3] = $match[1];
-							$tokens[$match[1]][3] = $key;
+							$token->setMatch($match[1]);
+							$match[2]->setMatch($key);
 						}
 					} else {
-						$tokens[$key][3] = false;
+						$token->setMatch(false);
 					}
 					break;
 
 				case T_BAD_CHARACTER:
-					$this->setReason('T_BAD_CHARACTER encountered on line ' . $this->getLine($token));
+					$this->setReason('T_BAD_CHARACTER encountered on line ' . $token->line);
 					break;
 
 				default:
 			}
-
-			$lastLineNumber = $token[2] + substr_count($token[1], PHP_EOL);
 		}
 
 		if (count($matchStack)) {
-			$this->setReason('Unmatched braces left on stack.  Last one was ' . $this->tokenToString($matchStack[0][1]));
+			$this->setReason('Unmatched braces left on stack.  Last one was ' . $matchStack[0][2]);
 		}
 
 		// 1 open tag at the end is fine
@@ -698,7 +496,7 @@ class Tokenizer implements ArrayAccess, Iterator {
 			$this->setReason('Two open PHP tags that were not closed');
 		}
 
-		return $tokens;
+		return $tokenObjects;
 	}
 
 
@@ -730,19 +528,6 @@ class Tokenizer implements ArrayAccess, Iterator {
 		$tokens = @token_get_all($str);
 		$tokenizer = new Tokenizer($tokens);
 		return $tokenizer;
-	}
-
-
-	/**
-	 * Returns a token as a string
-	 *
-	 * @param array|integer Token or absolute offset
-	 * return string
-	 */
-	protected function tokenToString($token) {
-		$str = $this->getName($token);
-		$str .= ' on line ' . $this->getLine($token);
-		return $str;
 	}
 
 
---
+++ tokenizer/tokenizertoken.class.php
@@ -0,0 +1,301 @@
+<?php
+
+/*
+ Copyright (c) 2011 individual committers of the code
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a
+ copy of this software and associated documentation files (the "Software"),
+ to deal in the Software without restriction, including without limitation
+ the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ and/or sell copies of the Software, and to permit persons to whom the
+ Software is furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ DEALINGS IN THE SOFTWARE.
+ 
+ Except as contained in this notice, the name(s) of the above copyright
+ holders shall not be used in advertising or otherwise to promote the sale,
+ use or other dealings in this Software without prior written authorization.
+ 
+ The end-user documentation included with the redistribution, if any, must 
+ include the following acknowledgment: "This product includes software
+ developed by contributors", in the same place and form as other third-party
+ acknowledgments. Alternately, this acknowledgment may appear in the software
+ itself, in the same form and location as other such third-party
+ acknowledgments.
+ */
+ 
+/**
+ * Make a small object for tokens so people don't need to remember which
+ * key in the array is the line number.
+ *
+ *   array(
+ *     [0] => T_TOKEN_CONSTANT,
+ *     [1] => 'token content',
+ *     [2] => 77,  // Line number
+ *     [3] => 28,  // Matching token offset or null if no match
+ *   )
+ *
+ * The matching token offset would give you the index of a '{' if you were
+ * at a '}'.  Usually it will be the same as the key of the token.  Only for
+ * special tokens that could have a match would it be different.
+ *
+ * This class uses arrays instead of potentially thousands of objects because
+ * it needs to run really fast on extremely large files.  That's why you will
+ * see a little duplicate code here.  I'd rather save a function call and
+ * prefer to call isset() instead of $this->valid().
+ */
+
+class TokenizerToken {
+	const EXCEPTION_PROPERTY = 608027;
+	protected $type;  // Token constant
+	protected $content;  // Content from PHP source file
+	protected $line;  // Line number
+	protected $match;  // Matching token's index
+	static protected $backtickIsLeft = true;
+	static protected $customTokens = array(
+		// Eliminate the only Hebrew token
+		T_PAAMAYIM_NEKUDOTAYIM => 'T_DOUBLE_COLON',
+
+		// Default token and ones that don't match strings
+		-1 => 'T_TOKENIZER_DEFAULT',
+		-2 => 'T_TOKENIZER_BACKTICK_LEFT',
+		-3 => 'T_TOKENIZER_BACKTICK_RIGHT',
+
+		// Ones that don't come back as arrays
+		'`' => 'T_TOKENIZER_BACKTICK',
+		'~' => 'T_TOKENIZER_BITWISE_NOT',
+		'^' => 'T_TOKENIZER_BITWISE_XOR',
+		'!' => 'T_TOKENIZER_BOOLEAN_NEGATION',
+		'{' => 'T_TOKENIZER_BRACE_LEFT',
+		'}' => 'T_TOKENIZER_BRACE_RIGHT',
+		'[' => 'T_TOKENIZER_BRACKET_LEFT',
+		']' => 'T_TOKENIZER_BRACKET_RIGHT',
+		':' => 'T_TOKENIZER_COLON',
+		',' => 'T_TOKENIZER_COMMA',
+		'.' => 'T_TOKENIZER_CONCAT',
+		'"' => 'T_TOKENIZER_DOUBLE_QUOTE',
+		'=' => 'T_TOKENIZER_EQUAL',
+		'>' => 'T_TOKENIZER_IS_GREATER_THAN',
+		'<' => 'T_TOKENIZER_IS_LESS_THAN',
+		'/' => 'T_TOKENIZER_MATH_DIVIDE',
+		'-' => 'T_TOKENIZER_MATH_MINUS',
+		'*' => 'T_TOKENIZER_MATH_MULTIPLY',
+		'%' => 'T_TOKENIZER_MATH_MODULUS',
+		'+' => 'T_TOKENIZER_MATH_PLUS',
+		'(' => 'T_TOKENIZER_PAREN_LEFT',
+		')' => 'T_TOKENIZER_PAREN_RIGHT',
+		'&' => 'T_TOKENIZER_REFERENCE',
+		';' => 'T_TOKENIZER_SEMICOLON',
+		'@' => 'T_TOKENIZER_SUPPRESS_ERRORS',
+		'?' => 'T_TOKENIZER_TERNARY',
+		'$' => 'T_TOKENIZER_VARIABLE_VARIABLE',
+
+		// T_STRING substitutions into tokens
+		'define' => 'T_TOKENIZER_DEFINE',
+		'false' => 'T_TOKENIZER_FALSE',
+		'true' => 'T_TOKENIZER_TRUE',
+	);
+	static protected $unknownTokenName = null;
+
+
+	/**
+	 * Create a Tokenizer object from a list of tokens
+	 *
+	 * @param array $tokens Token list from token_get_all()
+	 */
+	protected function __construct($type, $content, $line) {
+		$this->type = $type;
+		$this->content = $content;
+		$this->line = $line;
+		$this->match = null;  // No match needed
+	}
+
+
+	/**
+	 * Get any property via magic
+	 */
+	public function __get($name) {
+		switch ($name) {
+			case 'type':
+			case 'content':
+			case 'line':
+			case 'match':
+				return $this->$name;
+
+			case 'important':
+				return $this->isImportant();
+
+			case 'name':
+				return $this->getName();
+		}
+
+		throw new InvalidArgumentException($name . ' is not a property you can access', static::EXCEPTION_PROPERTY);
+	}
+
+
+	/**
+	 * Returns a token as a string
+	 *
+	 * return string
+	 */
+	public function __toString() {
+		$str = $this->getName() . ' on line ' . $this->line;
+		return $str;
+	}
+
+
+	/**
+	 * Convert one element from the output from tokens_get_all() into a
+	 * TokenizerToken object.
+	 *
+	 * @param array|string $data
+	 * @param null|object $previousToken
+	 * @return TokenizerToken
+	 */
+	static public function create($data, $previousToken) {
+		if (is_array($data)) {
+			// Some strings get converted into tokens
+			if (T_STRING === $data[0]) {
+				$lowercase = strtolower($data[1]);
+
+				if (isset(static::$customTokens[$lowercase])) {
+					$data[0] = $lowercase;
+				}
+			}
+
+			return new TokenizerToken($data[0], $data[1], $data[2]);
+		}
+
+		if (! $previousToken) {
+			$previousToken = new TokenizerToken(T_TOKENIZER_DEFAULT, '', 1);
+		}
+
+		$tokenType = $data;
+
+		if (! isset(static::$customTokens[$tokenType])) {
+			$tokenType = T_TOKENIZER_DEFAULT;
+		} elseif (T_TOKENIZER_BACKTICK == $tokenType) {
+			// Check for left/right backticks
+			if (static::$backtickIsLeft) {
+				$tokenType = T_TOKENIZER_BACKTICK_LEFT;
+			} else {
+				$tokenType = T_TOKENIZER_BACKTICK_RIGHT;
+			}
+
+			static::$backtickIsLeft = ! static::$backtickIsLeft;
+		}
+
+		$lastLine = $previousToken->line + substr_count($previousToken->content, PHP_EOL);
+
+		return new TokenizerToken($tokenType, $data, $lastLine);
+	}
+
+
+	public function getName() {
+		$type = $this->type;
+
+		if (! empty(static::$customTokens[$type])) {
+			return static::$customTokens[$type];
+		}
+
+		$name = token_name($type);
+
+		if ($name === static::$unknownTokenName) {
+			$name = 'UNKNOWN(' . $type. ')';
+		}
+
+		return $name;
+	}
+
+	
+	/**
+	 * Set up constants
+	 */
+	static public function initialize() {
+		foreach (static::$customTokens as $value => $name) {
+			if (! defined($name)) {
+				define($name, $value);
+			}
+		}
+
+		/* Set the actual unknown token name in case it's different
+		 * due to version changes or localization.
+		 */
+		static::$unknownTokenName = token_name(T_TOKENIZER_DEFAULT);
+	}
+
+
+	/**
+	 * Returns true if the current token is not one of the following:
+	 *
+	 * T_COMMENT
+	 * T_DOC_COMMENT
+	 * T_WHITESPACE
+	 *
+	 * @return boolean True if this token "counts" for processing
+	 */
+	public function isImportant() {
+		switch ($this->type) {
+			case T_COMMENT:
+			case T_DOC_COMMENT:
+			case T_WHITESPACE:
+				return false;
+		}
+
+		return true;
+	}
+
+
+	/**
+	 * Return true if this token is of the specified type.  If an array
+	 * is passed, will return true if this token is any of the specified
+	 * types.
+	 *
+	 * These are all equal
+	 *    $this->isType(T_COMMENT) || $this->isType(T_DOC_COMMENT)
+	 *    $this->isType(array(T_COMMENT, T_DOC_COMMENT))
+	 *    $this->isType(T_COMMENT, T_DOC_COMMENT)
+	 *
+	 * @param mixed|array Any token constant or array of constants
+	 * @return boolean
+	 */
+	public function isType($types) {
+		if (! is_array($types)) {
+			$types = func_get_args();
+		}
+
+		return in_array($this->type, $types);
+	}
+
+
+	/**
+	 * Reset the tokenizer class between tokenization of files
+	 *
+	 * You don't need to call this directly - the Tokenizer class does this.
+	 */
+	static public function reset() {
+		static::$backtickIsLeft = true;
+	}
+
+
+	/**
+	 * Sets the matching index.  Not really a property of a token,
+	 * but so handy and useful for the tokenizer.
+	 *
+	 * @param integer $match Index in Tokenizer
+	 */
+	public function setMatch($match) {
+		$this->match = $match;
+	}
+}
+
+TokenizerToken::initialize();
